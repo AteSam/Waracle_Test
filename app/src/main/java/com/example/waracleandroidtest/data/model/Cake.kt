be16da9b74acc7e78e65a3380e697ef06f682c92
AteSam <syedatesaam@gmail.com>
2021-10-20 15:35:03 +0100

    data class

---
+++ app/src/main/java/com/example/waracleandroidtest/data/model/Cake.kt
@@ -0,0 +1,9 @@
+package com.example.waracleandroidtest.data.model
+
+import com.google.gson.annotations.SerializedName
+
+data class Cake(
+    @SerializedName("title") val title: String,
+    @SerializedName("desc")  val desc: String,
+    @SerializedName("image") val image: String
+)
